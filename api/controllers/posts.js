65a9259b789f96ab2e24f4ebc6c2cb3e0ab8a597
kubilaycicek <kubilaycicek0693@gmail.com>
2021-01-02 02:01:47 +0300

    Blogify

---
+++ api/controllers/posts.js
@@ -0,0 +1,11 @@
+import Post from "../models/posts.js";
+
+
+export const getPosts = async (req, res) => {
+    try {
+        const posts = await Post.find();
+        res.status(200).json(posts);
+    } catch (err) {
+        res.status(404).json({ message: err.message });
+    }
+}
