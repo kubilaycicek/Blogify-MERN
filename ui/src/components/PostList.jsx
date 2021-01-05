2e8a423f861437ff4b4f92368f94be902cf6589e
kubilaycicek <kubilaycicek0693@gmail.com>
2021-01-05 22:02:40 +0300

    Added PostComponents

--- ui/src/components/PostList.jsx
+++ ui/src/components/PostList.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { makeStyles } from "@material-ui/core/styles";
+import { useSelector } from "react-redux";
+import { Grid, Button } from "@material-ui/core";
+import Post from "./Post";
+
+const useStyles = makeStyles((theme) => ({
+    layoutShifter: {
+        float: "right",
+        margin: theme.spacing(2),
+    },
+}));
 
 const PostList = () => {
+    const classes = useStyles();
+    const posts = useSelector((state) => state.posts.posts);
+
     return (
-        <div>
-                PostList knk
-        </div>
+        <>
+            
+        </>
     )
 }
 
